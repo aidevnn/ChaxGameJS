6f07a1b1361421230c4dc78c94554e6148626367
aidevnn <ilyas.kadassi@gmail.com>
2020-08-02 12:09:09 +0300

    Cube domination testcases

--- spec/testCube.spec.ts
+++ spec/testCube.spec.ts
@@ -1,3 +1,4 @@
+import { Content } from "../src/enum";
 import { Cube } from "./../src/cube";
 
 describe("Cube cells testing", () => {
@@ -28,5 +29,76 @@ describe("Cube cells testing", () => {
             expect(c0.Neighbors.length).toBe(c1.Neighbors.length, "Cell nb neighbors must be equal to AltCell nb neighbors.");
         }
     });
+});
 
+describe("Cube Domination testing.", () => {
+    it("Empty Cube.", () => {
+        const cube = new Cube();
+        const sc1 = cube.ComputeDomination(Content.P1);
+        const sc2 = cube.ComputeDomination(Content.P2);
+
+        expect(sc1.ScorePlayer).toBe(0);
+        expect(sc1.ScoreOpponent).toBe(0);
+        expect(sc2.ScorePlayer).toBe(0);
+        expect(sc2.ScoreOpponent).toBe(0);
+
+        expect(sc1.DomPlayer).toBe(0);
+        expect(sc1.DomOpponent).toBe(0);
+        expect(sc2.DomPlayer).toBe(0);
+        expect(sc2.DomOpponent).toBe(0);
+    });
+    
+    it("One Cell Cube.", () => {
+        const cube = new Cube();
+        cube.SetCellByKey("c000", Content.P1);
+        const sc1 = cube.ComputeDomination(Content.P1);
+        const sc2 = cube.ComputeDomination(Content.P2);
+
+        expect(sc1.ScorePlayer).toBe(1);
+        expect(sc1.ScoreOpponent).toBe(0);
+        expect(sc2.ScorePlayer).toBe(0);
+        expect(sc2.ScoreOpponent).toBe(1);
+
+        expect(sc1.DomPlayer).toBe(40);
+        expect(sc1.DomOpponent).toBe(0);
+        expect(sc2.DomPlayer).toBe(0);
+        expect(sc2.DomOpponent).toBe(40);
+    });
+    
+    it("Two Cells Cube (1).", () => {
+        const cube = new Cube();
+        cube.SetCellByKey("c000", Content.P1);
+        cube.SetCellByKey("c220", Content.P2);
+        const sc1 = cube.ComputeDomination(Content.P1);
+        const sc2 = cube.ComputeDomination(Content.P2);
+
+        expect(sc1.ScorePlayer).toBe(1);
+        expect(sc1.ScoreOpponent).toBe(1);
+        expect(sc2.ScorePlayer).toBe(1);
+        expect(sc2.ScoreOpponent).toBe(1);
+
+        expect(sc1.DomPlayer).toBe(28);
+        expect(sc1.DomOpponent).toBe(12);
+        expect(sc2.DomPlayer).toBe(28);
+        expect(sc2.DomOpponent).toBe(12);
+    });
+    
+    it("Two Cells Cube (2).", () => {
+        const cube = new Cube();
+        cube.SetCellByKey("c000", Content.P1);
+        cube.SetCellByKey("c200", Content.P2);
+        const sc1 = cube.ComputeDomination(Content.P1);
+        const sc2 = cube.ComputeDomination(Content.P2);
+
+        expect(sc1.ScorePlayer).toBe(1);
+        expect(sc1.ScoreOpponent).toBe(1);
+        expect(sc2.ScorePlayer).toBe(1);
+        expect(sc2.ScoreOpponent).toBe(1);
+
+        expect(sc1.DomPlayer).toBe(22);
+        expect(sc1.DomOpponent).toBe(18);
+        expect(sc2.DomPlayer).toBe(22);
+        expect(sc2.DomOpponent).toBe(18);
+    });
+    
 });
--- src/cube.ts
+++ src/cube.ts
@@ -1,14 +1,51 @@
-import { Dir, Content } from "./enum";
+import { Dir, Content, SameContent, GetOpponent } from "./enum";
 import { Cell, AltCell } from "./cell";
+
+export class CubeScore {
+    Player: Content;
+    DomPlayer: number;
+    DomOpponent: number;
+    ScorePlayer: number;
+    ScoreOpponent: number;
+    constructor(player: Content, scPlayer: number, scOpponent: number, domPlayer: number, domOpponent: number) {
+        this.Player = player;
+        this.ScorePlayer = scPlayer;
+        this.ScoreOpponent = scOpponent;
+        this.DomPlayer = domPlayer;
+        this.DomOpponent = domOpponent;
+    }
+
+    GetScore(c: Content): number {
+        if (c == Content.Empty)
+            return 0;
+
+        if (SameContent(c, this.Player))
+            return this.ScorePlayer;
+
+        return this.ScoreOpponent;
+    }
+
+    GetDomination(c: Content): number {
+        if (c == Content.Empty)
+            return 0;
+
+        if (SameContent(c, this.Player))
+            return this.DomPlayer;
+
+        return this.DomOpponent;
+    }
+}
 
 export class Cube {
     AllCells: Array<Cell>;
     AllAltCells: Array<AltCell>;
     Infos: Map<string, Cell>;
+    Queue: Array<AltCell>;
     constructor() {
         this.AllCells = [];
         this.AllAltCells = [];
         this.Infos = new Map();
+        this.Queue = [];
         this.Init();
     }
 
@@ -40,7 +77,7 @@ export class Cube {
                 let c1 = this.Infos.get(coords1.cxyz);
                 if (c1 == undefined)
                     continue;
-                    
+
                 c0.Neighbors.push(c1);
                 cc0.Neighbors.push(this.AllAltCells[c1.Id]);
 
@@ -52,5 +89,92 @@ export class Cube {
                 }
             }
         }
+    }
+
+    GetCellById(id: number): Cell {
+        return this.AllCells[id];
+    }
+
+    GetCellByKey(cxyz: string): Cell | null {
+        let c = this.Infos.get(cxyz);
+
+        if (c == undefined)
+            return null;
+
+        return c;
+    }
+
+    SetCellById(id: number, content: Content): void {
+        let c = this.AllCells[id];
+        if (c == undefined)
+            return;
+
+        c.Content = content;
+    }
+
+    SetCellByKey(cxyz: string, content: Content): void {
+        let c = this.Infos.get(cxyz);
+        if (c == undefined)
+            return;
+
+        c.Content = content;
+    }
+
+    ComputeDomination(player: Content): CubeScore {
+        let opponent = GetOpponent(player);
+
+        let ScorePlayer = 0;
+        let ScoreOpponent = 0;
+        let DomPlayer = 0;
+        let DomOpponent = 0;
+
+        this.Queue = [];
+
+        for (let i = 0; i < 24; ++i) {
+            var cc = this.AllAltCells[i];
+            cc.Content = this.AllCells[i].Content;
+            cc.AltContent = Content.Empty;
+            cc.Step = 0;
+
+            if (cc.Content != Content.Empty) {
+                this.Queue.push(cc);
+                cc.AltContent = cc.Content;
+
+                if (cc.Content == player) {
+                    DomPlayer += cc.Power;
+                    ++ScorePlayer;
+                }
+                else if (cc.Content == opponent) {
+                    DomOpponent += cc.Power;
+                    ++ScoreOpponent;
+                }
+            }
+        }
+
+        while (this.Queue.length != 0) {
+            var c = this.Queue.shift();
+            if (c == undefined)
+                break;
+
+            for (let n of c.Neighbors) {
+                if (n.Content == Content.Empty) {
+                    n.Content = c.Content;
+                    n.Step = c.Step + 1;
+                    this.Queue.push(n);
+
+                    if (n.Content == player) DomPlayer += n.Power;
+                    if (n.Content == opponent) DomOpponent += n.Power;
+                }
+                else if (n.Content != c.Content && n.AltContent == Content.Empty && n.Step == c.Step + 1) {
+                    n.AltContent = c.Content;
+                    if (n.Content == opponent && c.Content == player) {
+                        DomPlayer += n.Power;
+                        DomOpponent -= n.Power;
+                    }
+                }
+            }
+        }
+
+        return new CubeScore(player, ScorePlayer, ScoreOpponent, DomPlayer, DomOpponent);
     }
 }
